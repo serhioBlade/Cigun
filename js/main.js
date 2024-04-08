2d5532926d1bbee805572b2f9e1a99dcef6a8d1d
serhioBlade <serzh.khavanskiy@mail.ru>
2024-04-09 01:21:01 +0300

    beda

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    var location = window.location.href;
+    var cur_url = '' + location.split('/').pop();
+
+    $('.menu li').each(function () {
+        var link = $(this).find('a').attr('href');
+
+        if (cur_url == link) {
+            $(this).addClass('current');
+        }
+ 
+    });
+    console.log(cur_url);
+});
